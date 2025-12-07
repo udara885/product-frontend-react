d3a5be9fcd263a38047aceb17eddbe83f3925a3d
Udara Lakshan <ulakshan061@gmail.com>
2025-12-07 19:10:42 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 // https://vite.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_")
-  return defineConfig({
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-        },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.API_URL,
       },
     },
-  })
-}
+  },
+})

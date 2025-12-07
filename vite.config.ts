d80d01dff68157e8c75c59ce719dfc1917b69e95
Udara Lakshan <ulakshan061@gmail.com>
2025-12-07 18:31:51 +0530

    refactor: load environment variables for API URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_")
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+        },
       },
     },
-  },
-})
+  })
+}
